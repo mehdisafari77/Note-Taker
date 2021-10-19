1359dd10378a59aed2081f37c39bd08c5add6146
mehdisafari77 <sa.mehdisafari@gmail.com>
2021-10-19 15:14:57 -0700

    add routing to viewRouts.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const viewRouts = require("./routing/viewRouts")
 const app = express();
 const PORT = 3001;
 
@@ -6,6 +7,9 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
+app.use("/", viewRouts)
+
 app.listen(PORT, function() {
     console.log("App listening on http://localhost:" + PORT);
 });
+
